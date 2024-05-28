46123e56e1e60e08750f6fdc31f62a0cfc13d318
Warren Byron <wbyron@shoprite.co.za>
2024-05-28 17:07:09 +0100

    Add .env feature for deployment script

--- deploy-me.sh
+++ deploy-me.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Create a backend.env file if not exist
+# This will track the backend creation
+
+if [ ! -f backend.env ]; then
+    random=`echo $RANDOM | md5`
+    echo "backend="$random > backend.env
+fi
+
+source backend.env
+
+echo $backend
+
 cd terraform-backend
 terraform init
-terraform apply -auto-approve
+#terraform apply -auto-approve
